0e41f3faf60ad2dfaace0c05478fc36ea1daa989
Aditi Naithani <102923973+ditii-n@users.noreply.github.com>
2024-03-21 21:53:53 +0530

    Update Output.jsx

--- src/Components/Output.jsx
+++ src/Components/Output.jsx
@@ -7,7 +7,7 @@ function Output() {
   const fetchData = async () => {
     try {
       const response = await axios.get(
-        "http://localhost:8000/api/v1/users/getdata"
+        "https://aggressive-stockings-worm.cyclic.app/api/v1/users/getdata"
       );
       setFetchedData(response.data.message);
     } catch (error) {
